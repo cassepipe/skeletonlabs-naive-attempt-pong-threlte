a81abe575790a2153f647f9a3643eb74a3af005e
cassepipe <cassepipe@ymail.com>
2023-08-19 18:58:17 +0200

    better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,6 @@
 	import Pong from './Pong.svelte';
 </script>
 
-<div class="w-screen h-screen">
-	<Canvas frameloop="demand" debugFrameloop={false}>
-		<Pong />
-	</Canvas>
-	<p>Supposedly takes the width of parent element</p>
-</div>
+<Canvas frameloop="demand" debugFrameloop={false}>
+	<Pong />
+</Canvas>
--- src/routes/Ball.svelte
+++ src/routes/Ball.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { T } from "@threlte/core"
-	import type { MeshBasicMaterialParameters } from "three"
-	import type { Ball } from "./GameObjects"
+	import { T } from '@threlte/core';
+	import type { MeshBasicMaterialParameters } from 'three';
+	import type { Ball } from './GameObjects';
 
 	// Sizing
-	export let ball: Ball
-    console.log(`ball position: {x: ${ball.x}, y: ${ball.y}`)
+	export let ball: Ball;
+	console.log(`ball position: {x: ${ball.x}, y: ${ball.y}`);
 </script>
 
 <!-- Ball  -->
 <T.Mesh x={ball.x} y={ball.y}>
-	<T.BoxGeometry args={[ball.r, ball.r, 1]} />
+	<T.BoxGeometry args={[ball.r, ball.r, ball.r]} />
 	<T.MeshBasicMaterial args={[{ color: ball.color }]} />
 </T.Mesh>
